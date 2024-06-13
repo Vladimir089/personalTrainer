90fdb204aa32a6b7affdc619578ae9b6a0f1442a
Vladimir089 <pickerpikcer@gmail.com>
2024-06-13 19:17:21 +0300

    full 2

--- personalTrainer/Controller/Review/OneBoarding/WebViewController.swift
+++ personalTrainer/Controller/Review/OneBoarding/WebViewController.swift
@@ -9,25 +9,58 @@ import UIKit
 import WebKit
 import SnapKit
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, WKNavigationDelegate {
 
     var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        webView = WKWebView(frame: .zero)
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.navigationDelegate = self
         
         view.addSubview(webView)
         
         webView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-
+        
         if let url = URL(string: "https://www.google.com") {
             let request = URLRequest(url: url)
-            webView.showsLargeContentViewer = false
             webView.load(request)
         }
+    }
+  
+    // Сохранение куки-файлов
+    func saveCookies() {
+        webView.configuration.websiteDataStore.httpCookieStore.getAllCookies { cookies in
+            for cookie in cookies {
+                var cookieProperties = [HTTPCookiePropertyKey: Any]()
+                cookieProperties[.name] = cookie.name
+                cookieProperties[.value] = cookie.value
+                cookieProperties[.domain] = cookie.domain
+                cookieProperties[.path] = cookie.path
+                cookieProperties[.version] = cookie.version
+                cookieProperties[.expires] = cookie.expiresDate
+
+                let newCookie = HTTPCookie(properties: cookieProperties)
+                HTTPCookieStorage.shared.setCookie(newCookie!)
+            }
+        }
+    }
+    
+    // Загрузка куки-файлов
+    func loadCookies() {
+        let cookies = HTTPCookieStorage.shared.cookies
+        cookies?.forEach { cookie in
+            webView.configuration.websiteDataStore.httpCookieStore.setCookie(cookie)
+        }
+    }
+    
+    // Делегат метод, который вызывается после завершения загрузки
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // Сохраняем куки после полной загрузки страницы
+        saveCookies()
     }
 }
