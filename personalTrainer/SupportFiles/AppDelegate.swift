0b7fce596a0d695c5aed2a8e861c3f3e699dcb2b
Vladimir089 <pickerpikcer@gmail.com>
2024-06-19 18:58:28 +0300

    345

--- personalTrainer/SupportFiles/AppDelegate.swift
+++ personalTrainer/SupportFiles/AppDelegate.swift
@@ -1,5 +1,5 @@
 import UIKit
-import Firebase
+
 import SystemConfiguration.CaptiveNetwork
 import Network
 
@@ -13,8 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let navController = UINavigationController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        //FirebaseApp.configure()
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navController
